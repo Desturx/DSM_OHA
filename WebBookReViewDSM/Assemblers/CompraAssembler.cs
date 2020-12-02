678142fdfae429f1964c4978ca5af9e61dd2aed0
Alex <keyalex97@gmail.com>
2020-12-02 09:13:11 +0100

    Vista compra

---
+++ WebBookReViewDSM/Assemblers/CompraAssembler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookReViewGenNHibernate.En.BookReView;
+using WebBookReViewDSM.models;
+
+
+namespace WebBookReViewDSM.Assemblers
+{
+    public class CompraAssembler
+    {
+        public CompraViewModel ConvertEnToModelUI(CompraEn en)
+        {
+            CompraViewModel compra = new CompraViewModel();
+            compra.CompraID = en.CompraID;
+            compra.Fecha = en.Fechaped;
+            compra.Comercio = en.Comercio;
+            compra.Tipo_pago = en.Tipo_pago;
+
+            return compra;
+        }
+
+
+        public IList<CompraViewModel> ConvertListENToModel(IList<CompraEN> ens)
+        {
+            IList<CompraViewModel> compras = new List<CompraViewModel>();
+            foreach (CompraEN cp in ens)
+            {
+                compras.Add(ConvertEnToModelUI(cp));
+            }
+
+            return compras;
+        }
+    }
+}
---
+++ WebBookReViewDSM/Models/CompraViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBookReViewDSM.Models
+{
+    public class CompraViewModel
+    {
+        [ScaffoldColumn(false)]
+        public int CompraId { get; set; }
+
+        [Display(Prompt  ="Tipo de pago", Description="Forma de pago de la compra", Name="Tipo pago")]
+        [Required(ErrorMessage ="Debe indicar un tipo de pago")]
+        [StringLength(maximumLength: 200, ErrorMessage ="El tipo de pago es muy largo"]
+        public string Tipo_pago { get; set; }
+
+        [Display(Prompt = "Fecha", Description = "Fecha de la compra", Name = "Fecha")]
+        [Required(ErrorMessage = "Debe indicar un comercio")]
+        public string Fecha { get; set; }
+
+        [Display(Prompt = "Comercio", Description = "Plataforma sobre la que se ha comprado", Name = "Comercio")]
+        [Required(ErrorMessage = "Debe indicar un comercio")]
+        public string Comercio { get; set; }
+
+    }
+}
