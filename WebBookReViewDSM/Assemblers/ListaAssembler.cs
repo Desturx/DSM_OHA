bc7210f0ff0712358fdb9f2cf54e26bb60ac879e
Francisco Noguera Vidal <fnv1@mscloud.ua.es>
2020-12-04 18:08:52 +0100

    añadido lista

---
+++ WebBookReViewDSM/Assemblers/ListaAssembler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BookReViewGenNHibernate.En.BookReView;
+using WebBookReViewDSM.models;
+
+namespace WebBookReViewDSM.Assemblers
+{
+    public class ListaAssembler
+    {
+        public ListaViewModel ConvertEnToModelUI(ListaEn en)
+        {
+            ListaViewModel lista = new ListaViewModel();
+            lista.id = en.id;
+            compra.Tipolista = en.tipo;
+
+            return lista;
+        }
+
+        public IList<ListaViewModel> ConvertListENToModel(IList<ListaEN> ens)
+        {
+            IList<ListaViewModel> listas = new List<ListaViewModel>();
+            foreach (ListaEN cp in ens)
+            {
+                listas.Add(ConvertEnToModelUI(cp));
+            }
+
+            return listas;
+        }
+    }
+}
---
+++ WebBookReViewDSM/Models/ListaViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBookReViewDSM.Models
+{
+    public class Lista
+    {
+        [ScaffoldColumn(false)]
+        public int id { get; set; }
+
+        [Display(Prompt = "Tipo de lista", Description = "Estado actual de la lista", Name = "Tipo lista")]
+        public string Tipolista { get; set; }
+    }
+}
