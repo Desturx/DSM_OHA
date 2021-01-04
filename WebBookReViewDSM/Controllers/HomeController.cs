4a69e1e9782296e0526af00a1b7ffcfa89e716c1
Yera <ynms1@alu.ua.es>
2021-01-04 18:34:47 +0100

    Ya se ve la pagina (Login no funciona)

--- WebBookReViewDSM/Controllers/HomeController.cs
+++ WebBookReViewDSM/Controllers/HomeController.cs
@@ -3,33 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookReViewGenNHibernate.CAD.BookReview;
 using BookReViewGenNHibernate.CEN.BookReview;
 using BookReViewGenNHibernate.EN.BookReview;
-
+using WebBookReViewDSM.Assemblers;
+using WebBookReViewDSM.Models;
 
 namespace WebBookReViewDSM.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BasicController
     {
         public ActionResult Index()
         {
-            LibroCEN libCEN = new LibroCEN();
-            IEnumerable<LibroEN> listaArt = libCEN.ReadAll(0, -1).ToList();
-            return View(listaArt);
+            SessionInitialize();
+            LibroCAD libroCAD = new LibroCAD(session);
+            LibroCEN libCEN = new LibroCEN(libroCAD);
+
+            List<LibroEN> listaArt = libCEN.ReadAll(0, -1).ToList();
+            LibroAssembler lass = new LibroAssembler();
+            IEnumerable<LibroViewModel> list = lass.ConvertListENToModel(listaArt).ToList();
+            SessionClose();
+            return View(list);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-    }
-}
